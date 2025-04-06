8e543240b831132604ea28ef6deb4f7cbf09c596
Mr-Janith <rasiduanuhas32@gmail.com>
2025-04-06 21:47:33 +0530

    ✅ 𝙰𝙽𝙹𝚄 𝚇𝙿𝚁𝙾 ✅

--- session.js
+++ session.js
@@ -40,8 +40,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
-SUDO: process.env.SUDO || ["94717775628","94758775628"],
-PORT: process.env.PORT || "8000",
-FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n🌟 Premium Version | Advanced Features | Secure & Reliable 🌟\n© 2025 QUEEN ANJU XPRO. All rights reserved.",
-};
+    SESSION_ID: process.env.SESSION_ID || "your session id",
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
+    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
+    PORT: process.env.PORT || "8000",
+    SESSION_DIR: process.env.SESSION_DIR || "./session",
+    FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n\ud83c\udf1f Premium Version | Advanced Features | Secure & Reliable \ud83c\udf1f\n\u00a9 2025 QUEEN ANJU XPRO. All rights reserved." 
+  };
+  
