182ea3d367cdb8b6389ac68b7727edd08f411ffe
Mr-Janith <rasiduanuhas32@gmail.com>
2025-04-10 20:13:09 +0530

    ✅𝙰𝙽𝙹𝚄 𝚇𝙿𝚁𝙾✅

--- session.js
+++ session.js
@@ -45,7 +45,7 @@ module.exports = {
     GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
     PORT: process.env.PORT || "8000",
-    SESSION_DIR: process.env.SESSION_DIR || "./session",
+    SESSION_DIR: process.env.SESSION_DIR || "session",
     FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n\ud83c\udf1f Premium Version | Advanced Features | Secure & Reliable \ud83c\udf1f\n\u00a9 2025 QUEEN ANJU XPRO. All rights reserved." 
   };
   
