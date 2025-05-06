d45bfbaf22e5d8d7c954d58996d50e00c8e42eeb
wpmenuka <azuremake60@gmail.com>
2025-05-06 09:28:41 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,7 +40,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~6B1llKKS#qsmDqBbe_t7xwkGJOjMVqpjFxax3pDgVsPCxu5rb554",
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~GcsTFCRL#k26qpHzC5mqGgVzs7UOTU-C-KTlHO2qRxiFT41HSjiw",
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || "W5J76q2fOokYvmRJ73s2Pudjsj0TNh2hohyv",
     GITHUB_USERNAME: process.env.GITHUB_USERNAME || "wpmenuka",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94741321391", "94768504703","94755616554", "94762862143","94729224013","94771273552","94787437303","94761731783"],
